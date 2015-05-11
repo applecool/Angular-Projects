a5b648b9cc3173f528df4d92c4b8cd3989445d0d
applecool <kmitharsha@gmail.com>
2015-05-11 12:38:45 -0500

    added more gems

--- Gem Store/js/app.js
+++ Gem Store/js/app.js
@@ -4,12 +4,33 @@
 var app = angular.module('gemStore', [ ]); //the string gemStore should be matched exactly with the name of the ng-app in the html
 
     app.controller('StoreController', function(){
-        this.product = gem;
+        this.products = gems;
     });
     
-    var gem = {
+    var gems = [
+        
+        {
+        name: 'Pentagonal Gem',
+        price: 2000.99,
+        description: 'This gem is very rare and contains full of magic! Only a worthy person can use it',
+        canPurchase: true,
+        soldOut: false,
+        },
+        
+        {
+        name: 'Azurite',
+        price: 2222.99,
+        description: 'This gem is extracted from the rare species of shell animal pokemon Azurill, a water pokemon.',
+        canPurchase: true,
+        soldOut: false,
+        },
+        
+        {
         name: 'Dodecahedron',
-        price: 2.99,
+        price: 1299.99,
         description: 'This gem is mindblowing and sparkling! You cant take your eyes off it.',
-    };
+        canPurchase: true,
+        soldOut: false,
+        }
+    ];
 })();
