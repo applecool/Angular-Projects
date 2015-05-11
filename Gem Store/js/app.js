8d9ca5d1a330c6ad81ed1bb8c199ea72af2c3aa1
applecool <kmitharsha@gmail.com>
2015-05-11 12:17:34 -0500

    Added controller

--- Gem Store/js/app.js
+++ Gem Store/js/app.js
@@ -1,1 +1,15 @@
-var app = angular.module('gemStore', [ ]);
+
+//wrapping up javaScript in a closure is a good habit
+(function(){
+var app = angular.module('gemStore', [ ]); //the string gemStore should be matched exactly with the name of the ng-app in the html
+
+    app.controller('StoreController', function(){
+        this.product = gem;
+    });
+    
+    var gem = {
+        name: 'Dodecahedron',
+        price: 2.99,
+        description: 'This gem is mindblowing and sparkling! You cant take your eyes off it.',
+    };
+})();
