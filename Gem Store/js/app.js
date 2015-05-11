e90fc2c3ebd9ec47aa7e74ed3eadf2507f724c0a
applecool <kmitharsha@gmail.com>
2015-05-11 14:01:29 -0500

    Added resources and attributes

--- Gem Store/js/app.js
+++ Gem Store/js/app.js
@@ -13,8 +13,17 @@ var app = angular.module('gemStore', [ ]); //the string gemStore should be match
         name: 'Pentagonal Gem',
         price: 2000.99,
         description: 'This gem is very rare and contains full of magic! Only a worthy person can use it',
+        shine: 9,
+        rarity: 10,
+        color: '#EEE',
+        faces: 7,
         canPurchase: true,
         soldOut: false,
+        images:[
+            "images/gem-02.gif",
+            "images/gem-05.gif",
+            "images/gem-09.gif"
+        ],
         },
         
         {
@@ -22,7 +31,16 @@ var app = angular.module('gemStore', [ ]); //the string gemStore should be match
         price: 2222.99,
         description: 'This gem is extracted from the rare species of shell animal pokemon Azurill, a water pokemon.',
         canPurchase: true,
+        shine: 8,
+        rarity: 7,
+        color: '#CCC',
+        faces: 14,
         soldOut: false,
+        images:[
+            "images/gem-01.gif",
+            "images/gem-03.gif",
+            "images/gem-04.gif"
+        ],
         },
         
         {
@@ -30,7 +48,16 @@ var app = angular.module('gemStore', [ ]); //the string gemStore should be match
         price: 1299.99,
         description: 'This gem is mindblowing and sparkling! You cant take your eyes off it.',
         canPurchase: true,
+        shine: 7,
+        rarity: 7,
+        color: '#000',
+        faces: 12,    
         soldOut: false,
+        images:[
+            "images/gem-06.gif",
+            "images/gem-07.gif",
+            "images/gem-08.gif"
+        ],
         },
     ];
 })();
