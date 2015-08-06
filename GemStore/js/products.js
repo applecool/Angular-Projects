d5416f2d207113954f67f0ca1a25c386bd4a4fe5
applecool <kmitharsha@gmail.com>
2015-08-06 09:05:10 -0400

    added comments to the products.js

--- GemStore/js/products.js
+++ GemStore/js/products.js
@@ -1,33 +1,34 @@
 (function() {
   var app = angular.module('store-products', []);
+  //directive for productTitle
   app.directive('productTitle', function() {
     return {
       restrict: 'E',
       templateUrl: 'product-title.html'
     };
   });
-
+  //directive for productDescription
   app.directive('productDescription', function() {
     return {
       restrict: 'E',
       templateUrl: 'product-description.html'
     };
   });
-
+  //directive for productSpecs
   app.directive('productSpecs', function() {
     return {
       restrict: 'A',
       templateUrl: 'product-specs.html'
     };
   });
-
+  //directive for productReviews
   app.directive('productReviews', function() {
     return {
       restrict: 'E',
       templateUrl: 'product-reviews.html'
     };
   });
-
+  //directive for productTabs
   app.directive('productTabs', function() {
     return {
       restrict: 'E',
@@ -46,7 +47,7 @@
       controllerAs: 'Tab'
     };
   });
-
+  //directive for productGallery
   app.directive('productGallery', function() {
     return {
       restrict: 'E',
